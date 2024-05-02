0fea42e8757adb24693d281e98912cdef6be832d
Shravan Bhati <153543313+shravanbhati@users.noreply.github.com>
2024-05-03 03:12:35 +0530

    Update funzioni.js

--- funzioni.js
+++ funzioni.js
@@ -27,6 +27,6 @@
       var hide = document.getElementById("fakesite");
       hide.style.display = "none";
 	  show.style.display = "block";
-      document.getElementById("message").innerHTML = "<h1><strong><em>WELCOME TO SEE YOUR BHABHI HERE MUH DIKHAI!</em></strong></h1>"
+      document.getElementById("message").innerHTML = "<h1><strong><em>SEE YOUR BHABHI FOR MUH DIKHAI! 😊</em></strong></h1>"
    };
    
